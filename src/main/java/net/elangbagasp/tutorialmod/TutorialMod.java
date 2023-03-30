f9c44efacba5c362c0725be1ca01ac3fd39f7a26
elangbagaskaraputra <elangbagaskara.putra@gmail.com>
2023-03-30 10:41:00 +0700

    nyoba komen ges

--- src/main/java/net/elangbagasp/tutorialmod/TutorialMod.java
+++ src/main/java/net/elangbagasp/tutorialmod/TutorialMod.java
@@ -19,6 +19,7 @@ public class TutorialMod
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "tutorialmod";
     // Directly reference a slf4j logger
+    // Comment Test
     private static final Logger LOGGER = LogUtils.getLogger();
     public TutorialMod()
     {
